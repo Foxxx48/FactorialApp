6bf9948327d61b5cc37e5a429129c590d1c2e3fb
Foxxx48 <Foxxdim@mail.ru>
2022-10-11 19:02:55 +0300

    Use Android Unidirectional Data Flow Architecture (UDF)

--- app/src/main/java/com/fox/factorialapp/MainActivity.kt
+++ app/src/main/java/com/fox/factorialapp/MainActivity.kt
@@ -2,16 +2,47 @@ package com.fox.factorialapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.fox.factorialapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding ?: throw RuntimeException("ActivityMainBinding = null")
+
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        observeViewModels()
 
+        binding.button.setOnClickListener {
+            viewModel.calculate(binding.editTextNumber.text.toString())
+        }
+
+    }
+
+    private fun observeViewModels() {
+        viewModel.state.observe(this) {
+            if (it.isInProgress) {
+                binding.progressBar.visibility = View.VISIBLE
+                binding.button.isEnabled = false
+            } else {
+                binding.progressBar.visibility = View.GONE
+                binding.button.isEnabled = true
+            }
+            if(it.isError) {
+                Toast.makeText(this, "You did not enter a value", Toast.LENGTH_SHORT).show()
+            }
+
+                binding.textView.text = it.factorial
+        }
     }
 
 
---
+++ app/src/main/java/com/fox/factorialapp/MainViewModel.kt
@@ -0,0 +1,30 @@
+package com.fox.factorialapp
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    private val _state = MutableLiveData<State>()
+    val state: LiveData<State>
+        get() = _state
+
+
+
+    fun calculate(value: String?) {
+        _state.value = State(isInProgress = true)
+        if (value.isNullOrBlank()) {
+            _state.value = State(isError = true)
+            return
+        }
+        viewModelScope.launch {
+            val number = value.toLong()
+            delay(1000)
+            _state.value = State(factorial = number.toString())
+        }
+    }
+}
