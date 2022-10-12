18afea7d189ac28136be9c49c0cdd86df4c7dcc0
Foxxx48 <Foxxdim@mail.ru>
2022-10-12 10:21:24 +0300

    Add sealed Class State and used states in MainActivity

--- app/src/main/java/com/fox/factorialapp/MainActivity.kt
+++ app/src/main/java/com/fox/factorialapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import com.fox.factorialapp.Result
 import com.fox.factorialapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,31 +21,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         observeViewModels()
 
         binding.button.setOnClickListener {
             viewModel.calculate(binding.editTextNumber.text.toString())
         }
-
     }
 
     private fun observeViewModels() {
         viewModel.state.observe(this) {
-            if (it.isInProgress) {
-                binding.progressBar.visibility = View.VISIBLE
-                binding.button.isEnabled = false
-            } else {
-                binding.progressBar.visibility = View.GONE
-                binding.button.isEnabled = true
-            }
-            if(it.isError) {
-                Toast.makeText(this, "You did not enter a value", Toast.LENGTH_SHORT).show()
-            }
 
-                binding.textView.text = it.factorial
+            binding.progressBar.visibility = View.GONE
+            binding.button.isEnabled = true
+
+            when (it) {
+                is Progress -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                    binding.button.isEnabled = false
+                }
+                is Error -> {
+                    Toast.makeText(
+                        this,
+                        "You did not enter a value",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+                is Result -> {
+                    binding.textView.text = it.factorial
+                }
+            }
         }
     }
-
 
 
     override fun onDestroy() {
